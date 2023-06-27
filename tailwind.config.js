a3e66ad4576fed5fe75d4c01062f17fb2a14003b
Melops <95263124+Melonps@users.noreply.github.com>
2023-06-27 22:35:02 +0900

    [FIX] tailwind.config.jsの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./Components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         colors: {
             blue: {
